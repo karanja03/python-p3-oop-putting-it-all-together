9e0b67d7ecb1afcff3e21e4155fda5759651090d
karanja03 <wambuik03@gmail.com>
2023-08-21 13:35:17 +0300

    Complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+     '''Represents a book.'''
+     def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Use a private variable to store the page count
+
+        # Check if page_count is an integer when setting it
+        self.page_count = page_count
+
+     def get_page_count(self):
+        return self._page_count
+
+     def set_page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+     def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+    
+     page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    '''Represents a shoe.'''
+    
+class Shoe:
+    '''Represents a shoe.'''
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Use a private variable to store the size
+        self.size = size  # Check if size is an integer when setting it
+        self.condition = "New"  # Initialize condition attribute
+
+    def size(self):
+        return self._size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+ 
